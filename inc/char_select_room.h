2570df5ddd249ea4174a70f281753925109fa886
demodehavaianas <nil.obermuller@gmail.com>
2025-02-18 19:07:23 -0300

    melhorando o sistema da seleção de personagens com o sistema novo de inputs e organizando as funções do seletor.

--- inc/char_select_room.h
+++ inc/char_select_room.h
@@ -1,4 +1,4 @@
-#ifndef CHAR_SELECT_ROOM_H 
+#ifndef CHAR_SELECT_ROOM_H
 #define CHAR_SELECT_ROOM_H
 
 #include "types.h"
@@ -7,15 +7,12 @@
 #include "vdp_bg.h"
 #include "sprite_eng.h"
 #include "joy.h"
-
+#include "input_system.h"
 #include "estruturas.h"
 
-// Declaração das variáveis globais como extern 
+// Declaração das variáveis globais como extern
 extern u32 gFrames;
 extern u16 gInd_tileset;
-//extern PlayerOption pOptions[7];
-extern u16 prevInputP1, prevInputP2;
-extern u16 curInputP1, curInputP2;
 
 void processSelecaoPersonagens();
 
--- src/rooms/char_select_room.c
+++ src/rooms/char_select_room.c
@@ -2,127 +2,32 @@
 #include "stages.h"
 #include "sprites.h"
 
-Sprite *spr_p1_seletor; // Sprite de seleção de personagens
-Sprite *spr_p2_seletor; // Sprite de seleção de personagens
+void updateSelector(Player *player);
+
 Sprite *spr_p1_fighter; // Sprite de personagem no icone ativo
 Sprite *spr_p2_fighter; // Sprite de personagem no icone ativo
-u8 gP1Seletor, gP2Seletor;
 
-// KANO      76, 44
 // CAGE      20, 44
-// SUBZERO  188, 44
-// SONYA    244, 44
-// SCORPION 188, 108
-// LIU KANG 132, 108
+// KANO      76, 44
 // RAYDEN    76, 108
+// LIU KANG 132, 108
+// SUBZERO  188, 44
+// SCORPION 188, 108
+// SONYA    244, 44
 PlayerOption pOptions[7] = {
-    {76, 44},
     {20, 44},
-    {188, 44},
-    {244, 44},
-    {188, 108},
+    {76, 44},
+    {76, 108},
     {132, 108},
-    {76, 108}};
+    {188, 44},
+    {188, 108},
+    {244, 44},
+};
 
-void updateSelector(u8 *selector, Sprite *sprite, u16 curInput, u16 prevInput)
-{
-  if (*selector == 0)
-  { // KANO
-    if ((curInput & BUTTON_LEFT) && !(prevInput & BUTTON_LEFT))
-    {
-      SPR_setPosition(sprite, pOptions[1].x, pOptions[1].y);
-      *selector = 1;
-    }
-    else if ((curInput & BUTTON_RIGHT) && !(prevInput & BUTTON_RIGHT))
-    {
-      SPR_setPosition(sprite, pOptions[2].x, pOptions[2].y);
-      *selector = 2;
-    }
-    else if (curInput & BUTTON_DOWN)
-    {
-      SPR_setPosition(sprite, pOptions[6].x, pOptions[6].y);
-      *selector = 6;
-    }
-  }
-  else if (*selector == 1)
-  { // cage
-    if (curInput & BUTTON_RIGHT)
-    {
-      SPR_setPosition(sprite, pOptions[0].x, pOptions[0].y);
-      *selector = 0;
-    }
-  }
-  else if (*selector == 2)
-  { // subzero
-    if (curInput & BUTTON_LEFT)
-    {
-      SPR_setPosition(sprite, pOptions[0].x, pOptions[0].y);
-      *selector = 0;
-    }
-    else if (curInput & BUTTON_RIGHT)
-    {
-      SPR_setPosition(sprite, pOptions[3].x, pOptions[3].y);
-      *selector = 3;
-    }
-    else if (curInput & BUTTON_DOWN)
-    {
-      SPR_setPosition(sprite, pOptions[4].x, pOptions[4].y);
-      *selector = 4;
-    }
-  }
-  else if (*selector == 3)
-  { // sonya
-    if (curInput & BUTTON_LEFT)
-    {
-      SPR_setPosition(sprite, pOptions[2].x, pOptions[2].y);
-      *selector = 2;
-    }
-  }
-  else if (*selector == 4)
-  { // SCORPION
-    if (curInput & BUTTON_LEFT)
-    {
-      SPR_setPosition(sprite, pOptions[5].x, pOptions[5].y);
-      *selector = 5;
-    }
-    else if (curInput & BUTTON_UP)
-    {
-      SPR_setPosition(sprite, pOptions[2].x, pOptions[2].y);
-      *selector = 2;
-    }
-  }
-  else if (*selector == 5)
-  { // LIU
-    if (curInput & BUTTON_LEFT)
-    {
-      SPR_setPosition(sprite, pOptions[6].x, pOptions[6].y);
-      *selector = 6;
-    }
-    else if (curInput & BUTTON_RIGHT)
-    {
-      SPR_setPosition(sprite, pOptions[4].x, pOptions[4].y);
-      *selector = 4;
-    }
-  }
-  else if (*selector == 6)
-  { // RAYDEN
-    if (curInput & BUTTON_UP)
-    {
-      SPR_setPosition(sprite, pOptions[0].x, pOptions[0].y);
-      *selector = 0;
-    }
-    else if (curInput & BUTTON_RIGHT)
-    {
-      SPR_setPosition(sprite, pOptions[5].x, pOptions[5].y);
-      *selector = 5;
-    }
-  }
-}
-
-// void desenrolarBackground() { 
-//   for (u16 y = 0; y < 28; y+=4) { 
-//     VDP_setTileMapEx(BG_A, stage_char_select.tilemap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, gInd_tileset), 0, y, 0, y, 40, 4, DMA_QUEUE); 
-//     PAL_setPalette(PAL1, stage_char_select.palette->data, DMA); 
+// void desenrolarBackground() {
+//   for (u16 y = 0; y < 28; y+=4) {
+//     VDP_setTileMapEx(BG_A, stage_char_select.tilemap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, gInd_tileset), 0, y, 0, y, 40, 4, DMA_QUEUE);
+//     PAL_setPalette(PAL1, stage_char_select.palette->data, DMA);
 //     SYS_doVBlankProcess(); // Sincroniza com o VBlank para criar o efeito gradual
 //   }
 // }
@@ -132,11 +37,12 @@ void processSelecaoPersonagens()
   if (gFrames == 1)
   {
     VDP_setBackgroundColor(0);
+
+    // desenrolarBackground();
+
     VDP_loadTileSet(stage_char_select_a.tileset, gInd_tileset, DMA);
     VDP_setTileMapEx(BG_A, stage_char_select_a.tilemap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, gInd_tileset), 0, 0, 0, 0, 40, 28, DMA_QUEUE);
     PAL_setPalette(PAL0, char_select_a_pal.data, DMA);
-
-    // desenrolarBackground();
     gInd_tileset += stage_char_select_a.tileset->numTile;
 
     VDP_loadTileSet(stage_char_select_b.tileset, gInd_tileset, DMA);
@@ -144,53 +50,157 @@ void processSelecaoPersonagens()
     PAL_setPalette(PAL1, char_select_b_pal.data, DMA);
     gInd_tileset += stage_char_select_b.tileset->numTile;
 
-    spr_p1_seletor = SPR_addSprite(&player_seletor, pOptions[0].x, pOptions[0].y, TILE_ATTR(PAL1, FALSE, FALSE, FALSE));
+    player[0].sprite = SPR_addSprite(&player_seletor, pOptions[KANO].x, pOptions[KANO].y, TILE_ATTR(PAL1, FALSE, FALSE, FALSE));
     PAL_setPalette(PAL1, char_select_b_pal.data, DMA);
-    SPR_setDepth(spr_p1_seletor, 1);
-    gP1Seletor = 0;
+    SPR_setDepth(player[0].sprite, 1);
 
-    spr_p2_seletor = SPR_addSprite(&player_seletor, pOptions[2].x, pOptions[2].y, TILE_ATTR(PAL1, FALSE, FALSE, FALSE));
+    player[0].direcao = 1;
+    player[0].id = KANO;
+
+    player[1].sprite = SPR_addSprite(&player_seletor, pOptions[SUBZERO].x, pOptions[SUBZERO].y, TILE_ATTR(PAL1, FALSE, FALSE, FALSE));
     PAL_setPalette(PAL1, char_select_b_pal.data, DMA);
-    SPR_setAnim(spr_p2_seletor, 1);
-    SPR_setDepth(spr_p2_seletor, 2);
-    gP2Seletor = 2;
+    SPR_setAnim(player[1].sprite, 1);
+    SPR_setDepth(player[1].sprite, 2);
+
+    player[1].direcao = -1;
+    player[1].id = SUBZERO;
   }
 
-  curInputP1 = JOY_readJoypad(JOY_1);
-  curInputP2 = JOY_readJoypad(JOY_2);
-
-  updateSelector(&gP1Seletor, spr_p1_seletor, curInputP1, prevInputP1);
-  updateSelector(&gP2Seletor, spr_p2_seletor, curInputP2, prevInputP2);
-
-  if (gP1Seletor == 2 && !spr_p1_fighter)
+  for (int ind = 0; ind < 2; ind++)
   {
-
-    // spr_p1_fighter = SPR_addSprite(&spr_subzero, 7, 61, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
-    spr_p1_fighter = SPR_addSprite(&spr_subzero, 24, 104, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
-    PAL_setPalette(PAL2, spr_subzero.palette->data, DMA);
-    SPR_setAnim(spr_p1_fighter, 0);
-    SPR_setDepth(spr_p1_fighter, 0);
-    // SPR_update();
+    updateSelector(&player[ind]);
   }
-  else if (gP1Seletor!= 2 && spr_p1_fighter)
+
+  // TODO: ajustar isso depois e remover
+  if (player[0].id != SUBZERO && spr_p1_fighter)
   {
     SPR_releaseSprite(spr_p1_fighter);
     spr_p1_fighter = NULL;
   }
-
-    if (gP2Seletor == 2 && !spr_p2_fighter)
+  else if (player[0].id == SUBZERO && !spr_p1_fighter)
   {
-
+    spr_p1_fighter = SPR_addSprite(&spr_subzero, 24, 104, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
+    PAL_setPalette(PAL2, spr_subzero.palette->data, DMA);
+    SPR_setAnim(spr_p1_fighter, 0);
+    SPR_setDepth(spr_p1_fighter, 0);
+  }
+  if (player[1].id == SUBZERO && !spr_p2_fighter)
+  {
     // spr_p2_fighter = SPR_addSprite(&spr_subzero, 153, 61, TILE_ATTR(PAL3, FALSE, FALSE, TRUE));
     spr_p2_fighter = SPR_addSprite(&spr_subzero, 168, 104, TILE_ATTR(PAL3, FALSE, FALSE, TRUE));
     PAL_setPalette(PAL3, spr_subzero.palette->data, DMA);
     SPR_setAnim(spr_p2_fighter, 0);
     SPR_setDepth(spr_p2_fighter, 0);
-    // SPR_update();
   }
-  else if (gP2Seletor!= 2 && spr_p2_fighter)
+  else if (player[1].id != SUBZERO && spr_p2_fighter)
   {
     SPR_releaseSprite(spr_p2_fighter);
     spr_p2_fighter = NULL;
+  }
+
+  char stri[64];
+  sprintf(stri, "p1: %d", player[0].id);
+  VDP_drawText(stri, 1, 1);
+  sprintf(stri, "p2: %d", player[1].id);
+  VDP_drawText(stri, 1, 2);
+}
+
+void updateSelector(Player *player)
+{
+  switch (player->id)
+  {
+  case KANO:
+
+    if (player->key_JOY_LEFT_status == 1) // se apertar para esquerda, seleciona CAGE
+    {
+      SPR_setPosition(player->sprite, pOptions[CAGE].x, pOptions[CAGE].y);
+      player->id = CAGE;
+    }
+    else if (player->key_JOY_RIGHT_status == 1) // se apertar para direita, Subzero
+    {
+      SPR_setPosition(player->sprite, pOptions[SUBZERO].x, pOptions[SUBZERO].y);
+      player->id = SUBZERO;
+    }
+    else if (player->key_JOY_DOWN_status == 1) // se apertar para baixo, Raiden
+    {
+      SPR_setPosition(player->sprite, pOptions[RAIDEN].x, pOptions[RAIDEN].y);
+      player->id = RAIDEN;
+    }
+    break;
+
+  case CAGE:
+
+    if (player->key_JOY_RIGHT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[KANO].x, pOptions[KANO].y);
+      player->id = KANO;
+    }
+    break;
+
+  case SUBZERO:
+
+    if (player->key_JOY_LEFT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[KANO].x, pOptions[KANO].y);
+      player->id = KANO;
+    }
+    else if (player->key_JOY_RIGHT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[SONYA].x, pOptions[SONYA].y);
+      player->id = SONYA;
+    }
+    else if (player->key_JOY_DOWN_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[SCORPION].x, pOptions[SCORPION].y);
+      player->id = SCORPION;
+    }
+
+    break;
+  case SONYA:
+    if (player->key_JOY_LEFT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[SUBZERO].x, pOptions[SUBZERO].y);
+      player->id = SUBZERO;
+    }
+    break;
+  case SCORPION:
+    if (player->key_JOY_LEFT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[LIU_KANG].x, pOptions[LIU_KANG].y);
+      player->id = LIU_KANG;
+    }
+    else if (player->key_JOY_UP_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[SUBZERO].x, pOptions[SUBZERO].y);
+      player->id = SUBZERO;
+    }
+    break;
+  case LIU_KANG:
+    if (player->key_JOY_RIGHT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[SCORPION].x, pOptions[SCORPION].y);
+      player->id = SCORPION;
+    }
+    else if (player->key_JOY_LEFT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[RAIDEN].x, pOptions[RAIDEN].y);
+      player->id = RAIDEN;
+    }
+    break;
+  case RAIDEN:
+    if (player->key_JOY_RIGHT_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[LIU_KANG].x, pOptions[LIU_KANG].y);
+      player->id = LIU_KANG;
+    }
+    else if (player->key_JOY_UP_status == 1)
+    {
+      SPR_setPosition(player->sprite, pOptions[KANO].x, pOptions[KANO].y);
+      player->id = KANO;
+    }
+    break;
+
+  default:
+    break;
   }
 }
