da3f6b95dc35164d4a2fe3c0e3c3722a22011209
demodehavaianas <nil.obermuller@gmail.com>
2025-11-10 00:43:56 -0300

    Atualizando Game Vars

--- inc/game_vars.h
+++ inc/game_vars.h
@@ -4,6 +4,7 @@
 #include <genesis.h>
 #include "estruturas.h"
 
+extern u8 gRoom; // Sala atual
 extern u8 gAlturaDoPiso; // Altura do chão (posição Y dos personagens)
 extern u16 gInd_tileset; // Variable used to load background data
 extern u16 gBG_Width;    // Largura do Cenario em pixels
@@ -11,6 +12,7 @@ extern u16 gBG_Height;   // Altura do Cenario em pixels
 extern s16 gScrollValue; // Scrolling de Cenario
 extern u32 gFrames;      // Frame Counter
 extern bool gASG_system;
+extern u8 selectorBlinkTimer[2];//TODO: talvez precise em outra room.
 
 extern GraphicElement GE[25]; // Vetor global de elementos gráficos (GE)
 extern Player player[2];      // Vetor global de jogadores (Player 1 e Player 2)
